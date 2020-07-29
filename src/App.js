1c7e8c7e472073269e68e75f2e5b9c230c668c18
brunoamaia <brunoamaia94@gmail.com>
2020-07-29 14:00:31 -0300

    Ajuste do conteudo na Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import Menu from './components/Menu'  // Como o arquivo tem o nome de "index", não precisa colocar o nome
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Menu />
+
+    <BannerMain 
+      videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
+      url = {dadosIniciais.categorias[0].videos[0].url}
+      videoDescription = {"O que é Front-end? Trabalhando na área"}
+    />
+
+    <Carousel 
+      ignoreFirstVideo
+      category = {dadosIniciais.categorias[0]}
+    />
+
+    <Carousel 
+      ignoreFirstVideo
+      category = {dadosIniciais.categorias[1]}
+    />
+
+    <Carousel 
+      ignoreFirstVideo
+      category = {dadosIniciais.categorias[2]}
+    />
+
+    <Carousel 
+      ignoreFirstVideo
+      category = {dadosIniciais.categorias[3]}
+    />
+
+    <Carousel 
+      ignoreFirstVideo
+      category = {dadosIniciais.categorias[4]}
+    />
+
+    <Carousel 
+      ignoreFirstVideo
+      category = {dadosIniciais.categorias[5]}
+    />
+
     </div>
   );
 }
