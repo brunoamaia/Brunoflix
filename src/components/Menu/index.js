15efb0675e2275f4e0ab20ad0c6efd1d75d081d1
brunoamaia <brunoamaia94@gmail.com>
2020-07-29 11:30:16 -0300

    Menu de navegação

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Logo from '../../assests/img/logo.png'
+import './Menu.css'
 
 function Menu() {
     return (
-        <header>
-            Brunoflix, Show!
-        </header>
-    )
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Brunoflix logo" />
+            </a>
+
+            <a className="ButtonLink" href="/">
+                Novo Vídeo
+            </a>
+        </nav>
+    );
 }
 
 export default Menu 
