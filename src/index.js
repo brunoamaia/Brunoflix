40fe4cc7e337e7785347b91624922ae810548dc0
brunoamaia <brunoamaia94@gmail.com>
2020-07-30 10:12:28 -0300

    Rotas para a página 404 e cadastro de video

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import CadastroVideo from './components/pages/cadastro/video'
+const Pagina404 = ()=>( <div> Página 404 </div> )
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Switch >
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={ Pagina404 } />
+    </Switch>
+  </BrowserRouter>,
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,*/
   document.getElementById('root')
 );
