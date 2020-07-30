d37674b6b35fea83d6c85c3e41c69de1640924ba
brunoamaia <brunoamaia94@gmail.com>
2020-07-30 10:55:14 -0300

    Estilo da página Default

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,31 @@
+// a PageDefault é um template (modelo) que será utilizado várias vezes
+import React from 'react'
+import Menu from '../Menu'
+import Footer from '../Footer'
+
+
+import styled from 'styled-components'
+
+const Main = styled.main`
+    color: var(--white);
+    flex: 1;
+    padding-top: 50px;
+    padding-left: 5%;
+    padding-right: 5%;
+`
+
+
+// Forma de pegar os componentes "abrindo os elementos"
+function PageDefault( {children} ) {
+    return (
+        <div>
+            <Menu />
+            <Main>
+                {children}
+            </Main>
+            <Footer />
+        </div>
+    )
+}
+
+export default PageDefault
--- src/components/pages/cadastro/video/index.js
+++ src/components/pages/cadastro/video/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import PageDefault from '../../../PageDefault'
+import { Link } from 'react-router-dom'
 
 function CadastroVideo() {
     return(
-      <div>
-        Página de Cadastro de Vídeo
-      </div>
+      <PageDefault>
+        <h1>Cadastro de Vídeo</h1>
+
+
+        <Link to="/cadastro/categoria">
+          Cadastrar Categoria
+        </Link>
+      </PageDefault>
     )
 }
 
